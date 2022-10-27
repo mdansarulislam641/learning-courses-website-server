9e76f2c5d6738930ac7bcd11f3e1bd86ec59b246
mdansarulislam641 <mdansarulislam641.com>
2022-10-27 08:20:19 +0600

    junior hero api server make done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const course = require('./data/course.json')
+const port = process.env.PORT || 7000
+app.use(cors())
+app.get('/',(req,res)=>{
+    res.send('hello word this is the main')
+})
+
+app.get('/courses',(req,res)=>{
+    res.send(course)
+})
+
+app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id
+    const one = course.find(n=>n.id === id)
+    res.send(one)
+})
+app.listen(port, ()=>{
+    console.log('connected to server')
+})
